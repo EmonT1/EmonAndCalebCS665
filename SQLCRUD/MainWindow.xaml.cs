4a7d5bdbd3a588b038c553da12073178930256e2
etehrani <etehrani@DEVICENET>
2023-04-24 02:00:01 -0500

    Made some UI changes

--- SQLCRUD/MainWindow.xaml.cs
+++ SQLCRUD/MainWindow.xaml.cs
@@ -32,10 +32,15 @@ namespace SQLCRUD
             Loadgrid();
         }
 
+        public static List<string> tablesList = new List<string>() { "Table 1", "Table 2", "Table 3" };
+       
+
+
         SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\ProjectModels;Initial Catalog=Example;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         public void clear()
         {
-            xcoord.Clear();
+            SQLTableComboBox.ItemsSource = tablesList;
+            //SQLTableComboBox.Clear()
             ycoord.Clear();
             DelID.Clear();
         }
@@ -48,7 +53,7 @@ namespace SQLCRUD
             SqlDataReader sdr = command.ExecuteReader();
             dt.Load(sdr);
             conn.Close();
-            coordgrid.ItemsSource = dt.DefaultView;
+            SQLTableInfoInDataGrid.ItemsSource = dt.DefaultView;
 
         }
         private void clearbtn_Click(object sender, RoutedEventArgs e)
@@ -57,7 +62,7 @@ namespace SQLCRUD
         }
         public bool isValid()
         {
-            if(xcoord.Text == string.Empty && ycoord.Text == string.Empty)
+            if(SQLTableComboBox.Text == string.Empty && ycoord.Text == string.Empty)
             {
                 MessageBox.Show("Coordinates are required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
                 return false;
@@ -72,7 +77,7 @@ namespace SQLCRUD
                 {
                     SqlCommand cmd = new SqlCommand("INSERT INTO [Table] VALUES (@xcoord, @ycoord)", conn);
                     cmd.CommandType = CommandType.Text;
-                    cmd.Parameters.AddWithValue("@xcoord", int.Parse(xcoord.Text));
+                    cmd.Parameters.AddWithValue("@xcoord", int.Parse(SQLTableComboBox.Text));
                     cmd.Parameters.AddWithValue("@ycoord", int.Parse(ycoord.Text));
                     conn.Open();
                     cmd.ExecuteNonQuery();
@@ -115,7 +120,7 @@ namespace SQLCRUD
         private void Updatebtn_Click(object sender, RoutedEventArgs e)
         {
             conn.Open();
-            SqlCommand cmd = new SqlCommand("update [Table] set xcoord = '" + xcoord.Text + "', ycoord = '" + ycoord.Text + "' WHERE ID = '" + DelID.Text + "'", conn);
+            SqlCommand cmd = new SqlCommand("update [Table] set xcoord = '" + SQLTableComboBox.Text + "', ycoord = '" + ycoord.Text + "' WHERE ID = '" + DelID.Text + "'", conn);
             try
             {
                 cmd.ExecuteNonQuery();
